3f8bcdff469498bae3f6810e10d4dc75767256a7
AlejandraVillada <villadatorresmariaalejandra@gmail.com>
2022-11-05 14:57:12 -0500

    modelo, controlador

--- src/vistas/login.php
+++ src/vistas/login.php
@@ -1,1 +1,20 @@
-<div>holsa</div>
+<div class="row container d-flex justify-content-center" style="width: 400px;">
+    <div class="card bg-primary-plantilla">
+        <div class="col">
+            <div class="row"> MACAK</div>
+            <div class="row"> Construyendo Familia</div>
+            <div class="row">
+                <form action="" method="post">
+                    <div class="form-group">
+                        <label for=""> Usuario</label>
+                        <input type="text" name="" placeholder="usuario" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label for=""> Contraseña</label>
+                        <input type="text" name="" placeholder="password" class="form-control">
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
